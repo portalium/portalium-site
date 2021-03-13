019e8dfb0150beda80537bfb2951eaa0e45cdf2e
demirmehmet0 <demirmehmet0@yahoo.com>
2021-03-13 15:42:59 +0300

    commit

--- src/views/frontend/home/index.php
+++ src/views/frontend/home/index.php
@@ -6,6 +6,6 @@ $this->title = Module::t('Home');
 ?>
 <div class="site-index">
     <div class="jumbotron">
-        <h1><?= Module::t('Portalium Anasayfa Home - Frontend') ?></h1>
+        <h1><?= Module::t('Portalium Ana Home - Frontend') ?></h1>
     </div>
 </div>
